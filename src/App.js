2aa8e46f9a93bfc4202c4b040085fd32b804d9ac
Pedro Kohler <pedro.kohler@dtidigital.com.br>
2021-05-16 15:26:56 -0300

    Hotfix: adapt routes for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import GamePage from './pages/game';
 import MenuPage from './pages/menu';
 
